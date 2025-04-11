d3f59d80da040f0b9482145f9bbd116b3cab25fe
Pathum-SC12402 <fernando12402@usci.ruh.ac.lk>
2025-04-11 21:32:46 +0530

    initialized my portfolio

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I'm an enthusiastic <b className="purple">Software Engineer</b> currently pursuing a Bachelor's in Computer Science at the University of Ruhuna.
+              <br />
+              <br />
+              I enjoy working across the full stack, with a strong passion for <i><b className="purple">backend development</b></i> using technologies like <b className="purple">Node.js, Spring Boot,</b> and <b className="purple">Express.js</b>.
+              <br />
+              <br />
+              I’m also deeply interested in areas like <i><b className="purple">DevOps, AI model training</b></i>, and <b className="purple">Machine Learning</b>, constantly looking to expand my knowledge and apply it in impactful ways.
+              <br />
+              <br />
+              My toolkit includes <b className="purple">JavaScript, Python, Java,</b> and <b className="purple">React.js</b>, with a keen eye for creating clean, scalable applications and collaborating on meaningful projects.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Pathum-SC12402"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://x.com/i/flow/login?redirect_after_login=%2FMGPD_fernando"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/pathum-sc12402"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/pathum6151"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
